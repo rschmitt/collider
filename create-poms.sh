f0730cfee728dbdacb62320c9eb7f9406840d977
Ryan Schmitt <rschmitt@pobox.com>
2015-06-08 14:35:16 -0700

    Accept a range of Clojure versions

--- create-poms.sh
+++ create-poms.sh
@@ -2,8 +2,8 @@
 
 set -eux
 
-cp pom.xml pom-1.7.xml
-cp pom.xml pom-1.5.xml
-
-perl -i -pe 's/1.6.0/1.7.0-RC1/g' pom-1.7.xml
-perl -i -pe 's/1.6.0/1.5.1/g' pom-1.5.xml
+for i in 1.5.1 1.6.0 1.7.0-RC1
+do
+    cp pom.xml pom-$i.xml
+    perl -i -pe 's/\[1.5.0,\)/'"$i"'/g' pom-$i.xml
+done
